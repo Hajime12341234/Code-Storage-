9f5d7a741b89c861749afb8a6fd60f543ccf7bdf
Hajime12341234 <132004336+Hajime12341234@users.noreply.github.com>
2024-01-05 21:19:30 -0500

    Create myFirstBlog.js
    This is going to be my first blog I am to create. I have not done anything on it as I do not have anything particular to talk about.

---
+++ myFirstBlog.js
@@ -0,0 +1,12 @@
+// myFirstBlog.js
+import React from 'react';
+
+const MyFirstBlog = () => {
+  return (
+    <div>
+      <h1>My First Blog Page Content</h1>
+    </div>
+  );
+}
+
+export default MyFirstBlog;
